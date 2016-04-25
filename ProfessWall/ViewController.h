97cc425abefb6d979fb4f74b0a2fdcd46abfda3c
曹家明 <marine@caojiamingdemacbook-pro.local>
2016-04-25 14:09:37 +0800

    Initial Commit

---
+++ ProfessWall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProfessWall
+//
+//  Created by 曹家明 on 16/4/25.
+//  Copyright © 2016年 caojiaming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProfessWall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProfessWall
+//
+//  Created by 曹家明 on 16/4/25.
+//  Copyright © 2016年 caojiaming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
